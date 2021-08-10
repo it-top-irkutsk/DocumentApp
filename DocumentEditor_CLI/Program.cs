b2fd834a4de307229783fe8387fef3fa321930fb
Starinin Andrey <starininandrey@gmail.com>
2021-08-10 14:26:40 +0300

    Созхдание консольного редактора

---
+++ DocumentEditor_CLI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DocumentLib;
+using DocumentLib.FileWorking;
+
+namespace DocumentEditor_CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\Stari\Desktop\BestOil.txt";
+            IFileWorking fileWorking;
+
+            fileWorking = new TxtFileWorking();
+            var document = fileWorking.Open(path);
+            Console.WriteLine(document.Content);
+        }
+    }
+}
